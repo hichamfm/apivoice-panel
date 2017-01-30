71e5beaddb62a0e7a2099db70c35e19d55b3f406
Jairo <kidandcat@gmail.com>
2017-01-30 13:24:31 +0100

    1

--- src/services/call/hooks/receive.js
+++ src/services/call/hooks/receive.js
@@ -10,38 +10,42 @@ const defaults = {};
 module.exports = function(options) {
   options = Object.assign({}, defaults, options);
 
-  return function(hook) {
+  return function(hook, next) {
     let configService = hook.app.service('configs');
-    console.log('DATA', hook.data);
 
+    configService.find().then((c) => {
 
-    if (!hook.data.userfield || hook.data.userfield === '') {
+      let config = c.data[0];
+
+      if (!hook.data.userfield || hook.data.userfield === '') {
         hook.result = {
-            "command": "speak_getdtmf",
-            "options": "netelip;Pedro;Introduce el pin de cuatro digitos;7000;4;1",
-            "userfield": "1"
+          "command": "speak_getdtmf",
+          "options": "netelip;Pedro;Introduce el pin de cuatro digitos;7000;4;1",
+          "userfield": "1"
         }
-    } else if (hook.data.userfield >= 1) {
+      } else if (hook.data.userfield >= 1) {
         if (hook.data.dtmf && hook.data.dtmf !== '' && hook.data.dtmf.length === 4) {
-            hook.result = J{
-                "command": "speak",
-                "options": "netelip;Pedro;Autenticacion correcta",
-                "userfield": "-1"
-            }
+          hook.result = J {
+            "command": "speak",
+            "options": "netelip;Pedro;Autenticacion correcta",
+            "userfield": "-1"
+          }
         } else {
-            hook.data.userfield++;
-            hook.result = {
-                "command": "speak_getdtmf",
-                "options": `netelip;Pedro;Intento numero ${hook.data.userfield}. Introdce el pin de cuatro digitos;7000;4;1`,
-                "userfield": ++hook.data.userfield
-            }
+          hook.data.userfield++;
+          hook.result = {
+            "command": "speak_getdtmf",
+            "options": `netelip;Pedro;Intento numero ${hook.data.userfield}. Introdce el pin de cuatro digitos;7000;4;1`,
+            "userfield": ++hook.data.userfield
+          }
         }
-    } else if (hook.data.userfield == -1) {
+      } else if (hook.data.userfield == -1) {
         hook.result = {
-            "command": "hangup",
-            "options": "",
-            "userfield": ""
+          "command": "hangup",
+          "options": "",
+          "userfield": ""
         }
-    }
+      }
+      next();
+    })
   };
 };
